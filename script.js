c0a35dd67cbd29980e62c68202b31ff6c8ce24ae
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-02-19 20:33:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+  let ism1=prompt("Ismingizni kiriting")
+  let familya=prompt("Familyangizni kiriting")
+  let manzilingiz=prompt("yashash joyingizni kiriting")
+  let maktabingiz=prompt("maktabingiz raqami")
+  let faningiz=prompt("yoqtirgan faningiz")
+  let sport=prompt("sport turingiz")
+  let jamoa=prompt("yoqtirgan jamoangiz")
+
+
+  console.log( "Sizning ismingiz:" + ism1);
+  console.log( "familyangiz:" + familya);
+  console.log( "Yashash joyingiz:" + manzilingiz);
+  console.log( "maktabingiz raqami:" + maktabingiz);
+  console.log( "yoqtirgan faningiz:" + faningiz);
+  console.log( "sport turingiz:" + sport);
+  console.log( "yoqtirgan jamoangiz:" + jamoa);
+  
